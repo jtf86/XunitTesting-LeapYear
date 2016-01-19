738c799beefe5350e086396990714f1bffbb245d
John <john@epicodus.com>
2016-01-19 11:03:01 -0800

    add fact declaration to repair bugs in testing

--- Tests/LeapYearTest.cs
+++ Tests/LeapYearTest.cs
@@ -4,6 +4,7 @@ namespace LeapYear
 {
   public class LeapYearTest
   {
+
     [Fact]
     public void IsLeapYear_ForNumberDivisibleByFour_true()
     {
@@ -11,10 +12,20 @@ namespace LeapYear
       LeapYear LeapYear = new LeapYear();
       Assert.Equal(true, LeapYear.IsLeapYear(2012));
     }
+
+    [Fact]
     public void IsLeapYear_ForNumberNotDivisibleByFour_false()
     {
       LeapYear LeapYear = new LeapYear();
       Assert.Equal(false, LeapYear.IsLeapYear(1999));
     }
+
+    [Fact]
+    public void IsLeapYear_ForMultiplesOfOneHundred_false()
+    {
+      LeapYear LeapYear = new LeapYear();
+      Assert.Equal(false, LeapYear.IsLeapYear(1900));
+    }
+    
   }
 }
